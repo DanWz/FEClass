1e250701c44ddbafa39a7518154bbbce146c53b2
DanW <bambinoking@gmail.com>
2022-08-02 23:16:25 -0600

    subdirectory1

---
+++ week1/VSTest1.js
@@ -0,0 +1,49 @@
+//Boolean = true or false just writing it out for good meansure
+//camelCase.
+
+var firstName = "Dan";
+var lastName = "Wilson";
+
+var numberOfFriends = 2;
+
+var isHotOutside = false;
+var isHappy = true;
+
+var customerFirstName = "Pete";
+var numberTest = 100.54
+var bankAccountBalance = numberTest;
+
+//write hello world in browser console
+console.log('Hello World');
+
+
+console.log(5 + 3);
+//testing if spaces are needed they are not.
+console.log(4-2);
+console.log(6*7);
+console.log(8/2);
+console.log(bankAccountBalance);
+
+//subtract number from bankAccountBalance.
+bankAccountBalance = bankAccountBalance -5;
+
+console.log(bankAccountBalance);
+
+//subtract number from bankAccountBalance.
+bankAccountBalance -= 5;
+
+console.log(bankAccountBalance);
+
+bankAccountBalance += 10
+
+console.log(bankAccountBalance);
+
+console.log(bankAccountBalance * numberOfFriends);
+
+console.log(5 == 5);
+console.log(5==6);
+console.log(bankAccountBalance == numberTest)
+
+
+
+
